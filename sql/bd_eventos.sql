4a39f2714cf113ad465b20855e04940275f8bf2d
Carloscastro16 <andycastro.001016@gmail.com>
2022-07-13 21:46:52 -0500

    Cambios

--- sql/bd_eventos.sql
+++ sql/bd_eventos.sql
@@ -3,40 +3,34 @@ Use db_eventos;
 /*--------------------------------TABLA USUARIO-------------------------*/
 CREATE TABLE `usuario` (
   `cod_usuario` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `fk_rol_usuario` INT UNSIGNED NOT NULL,
   `nombre_usuario` VARCHAR(45) NULL,
   `ape_paterno` VARCHAR(45) NULL,
   `ape_materno` VARCHAR(45) NULL,
   `correo_usuario` VARCHAR(100) NULL,
-  `contra_usuario` VARCHAR(45) NULL,
-  `fecha_nac` DATE NULL);
- /*--------------------------------TABLA USUARIOS EMPRESA-------------------------*/
-  CREATE TABLE `db_eventos`.`empresa` (
-  `cod_usuario_em` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre_empresa` VARCHAR(45) NULL,
-  `nom_representante` VARCHAR(45) NULL,
-  `ape_pa_representante` VARCHAR(45) NULL,
-  `ape_ma_representante` VARCHAR(45) NULL,
-  `giro_comercial` VARCHAR(45) NULL,
-  `tel_empresa` INT(10) NULL,
-  `correo_empresa` VARCHAR(100) NULL,
-  `contra_empresa` VARCHAR(45) NULL,
-  `direc_empresa` VARCHAR(500) NULL,
-  PRIMARY KEY (`cod_usuario_em`));
+  `contra_usuario` VARCHAR(200) NULL,
+  `tel_empresa` BIGINT(10) NULL,
+  `rfc` VARCHAR(50) NULL,
+  FOREIGN KEY (fk_rol_usuario) REFERENCES rol_usuario(cod_rol));
   /*--------------------------------TABLA TIPOS DE SERVICIO-------------------------*/
   CREATE TABLE `db_eventos`.`tipo_servicio` (
-`cod_tipo_servicio` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom_servicio` VARCHAR(20) NULL,
-  PRIMARY KEY (`cod_tipo_servicio`));
+	`cod_tipo_servicio` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`nom_servicio` VARCHAR(45) NULL,
+	PRIMARY KEY (`cod_tipo_servicio`));
+  /*--------------------------------TABLA ROLES-------------------------*/
+  CREATE TABLE `db_eventos`.`rol_usuario` (
+	`cod_rol` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`nom_rol` VARCHAR(45) NULL,
+	PRIMARY KEY (`cod_rol`));
   /*--------------------------------TABLA MONTAJE-------------------------*/
   CREATE TABLE `db_eventos`.`montaje` (
   `cod_montaje` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre_montaje` VARCHAR(30) NULL,
-  `descrip_montaje` VARCHAR(500) NULL,
   PRIMARY KEY (`cod_montaje`));
   /*--------------------------------TABLA PAQUETES DE EVENTOS-------------------------*/
   CREATE TABLE `db_eventos`.`paquete` (
   `cod_paquete` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `fk_cod_usuario_em` INT UNSIGNED NOT NULL,
+  `fk_cod_empresa` INT UNSIGNED NOT NULL,
   `fk_cod_tipo_servicio` INT UNSIGNED NOT NULL,
   `nom_paquete` VARCHAR(45) NULL,
   `descrip_paquete` VARCHAR(900) NULL,
@@ -45,7 +39,7 @@ CREATE TABLE `usuario` (
   `locacion_evento` VARCHAR(50) NULL,
   `direc_evento` VARCHAR(900) NULL,
   `disponibilidad_evento` VARCHAR(12) NULL,
-  FOREIGN KEY (fk_cod_usuario_em) REFERENCES empresa(cod_usuario_em),
+  FOREIGN KEY (fk_cod_empresa) REFERENCES usuario(cod_usuario),
   FOREIGN KEY (fk_cod_tipo_servicio) REFERENCES tipo_servicio(cod_tipo_servicio),
   PRIMARY KEY (`cod_paquete`));
   /*--------------------------------ORDEN DE EVENTO-------------------------*/
@@ -64,5 +58,21 @@ CREATE TABLE `db_eventos`.`orden_evento` (
 	FOREIGN KEY (fk_cod_paquete) REFERENCES paquete(cod_paquete),
   PRIMARY KEY (`cod_orden_evento`));
   
+/*-------------------------------- Triggers -------------------------*/
+DELIMITER $$
+CREATE TRIGGER tg_tipo_usuario
+AFTER INSERT ON usuario
+FOR EACH ROW
+BEGIN
+  IF isnull(NEW.rfc)
+    THEN
+      UPDATE usuario
+      set fk_rol_usuario = 2
+      WHERE cod_usuario = NEW.cod_usuario;
+  END IF ;
+END$$
+DELIMITER ;
+drop trigger tg_tipo_usuario;
 
-  
+INSERT INTO usuario(fk_rol_usuario, nombre_usuario ,ape_paterno ,ape_materno , correo_usuario, contra_usuario) 
+	VALUE (1,"Carlos Andre", "Castro", "Rodriguez", "andycastro.2716@gmail.com");
--- sql/sp.sql
+++ sql/sp.sql
@@ -14,3 +14,108 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+/* Llamado para registro de clientes */
+USE `db_eventos`;
+DROP procedure IF EXISTS `sp_reg_cliente`;
+
+USE `db_eventos`;
+DROP procedure IF EXISTS `db_eventos`.`sp_reg_cliente`;
+;
+
+DELIMITER $$
+USE `db_eventos`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_reg_cliente`(
+	in nom_usuario VARCHAR(45),
+    in apellPa VARCHAR(45),
+	in apellMa VARCHAR(45),
+    in correo VARCHAR(100),
+    in contra VARCHAR(200)
+    
+)
+BEGIN
+	INSERT INTO usuario(fk_rol_usuario, nombre_usuario ,ape_paterno ,ape_materno , correo_usuario, contra_usuario) 
+	VALUE (2,nom_usuario, apellPa, apellMa ,correo ,contra);
+END$$
+
+DELIMITER ;
+;
+
+/* Llamado para registro de Empresas */
+USE `db_eventos`;
+DROP procedure IF EXISTS `sp_reg_usrempresa`;
+
+USE `db_eventos`;
+DROP procedure IF EXISTS `db_eventos`.`sp_reg_usrempresa`;
+;
+
+DELIMITER $$
+USE `db_eventos`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_reg_usrempresa`(
+	in nom_usuario VARCHAR(45),
+    in apellPa VARCHAR(45),
+	in apellMa VARCHAR(45),
+    in correo VARCHAR(100),
+    in contra VARCHAR(200),
+    in celular BIGINT(10),
+    in rfc VARCHAR(50)
+)
+BEGIN
+	INSERT INTO usuario(fk_rol_usuario, nombre_usuario ,ape_paterno ,ape_materno , correo_usuario, contra_usuario, tel_empresa, rfc) 
+	VALUE (3, nom_usuario, apellPa, apellMa ,correo ,contra, celular, rfc);
+    
+END$$
+
+DELIMITER ;
+;
+/* Llamado para trigger login */
+USE `db_eventos`;
+DROP procedure IF EXISTS `tr_logeo_usuarios`;
+
+USE `db_eventos`;
+DROP procedure IF EXISTS `db_eventos`.`tr_logeo_usuarios`;
+;
+
+DELIMITER $$
+USE `db_eventos`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tr_logeo_usuarios`(
+	in correo_tr varchar(100),
+    in id_usuario_tr INT(10),
+    in rol_usuario_tr INT(10)
+)
+BEGIN
+	INSERT INTO historial_logeo(Accion, correo) values (
+		concat("El usuario con id: ", id_usuario_tr,"y con el rol: ",rol_usuario_tr ," inicio sesión"),
+        correo_tr
+    );
+    
+END$$
+
+DELIMITER ;
+;
+
+/* Llamado para trigger logout */
+USE `db_eventos`;
+DROP procedure IF EXISTS `tr_salida_usuario`;
+
+USE `db_eventos`;
+DROP procedure IF EXISTS `db_eventos`.`tr_salida_usuario`;
+;
+
+DELIMITER $$
+USE `db_eventos`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tr_salida_usuario`(
+	in correo_tr varchar(100),
+    in id_usuario_tr INT(10),
+    in rol_usuario_tr INT(10)
+)
+BEGIN
+	INSERT INTO historial_logeo(Accion, correo) values (
+		concat("El usuario con id: ", id_usuario_tr," y con el rol: ",rol_usuario_tr ," cerro sesión"),
+        correo_tr
+    );
+    
+END$$
+
+DELIMITER ;
+;
