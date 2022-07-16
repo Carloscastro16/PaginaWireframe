71621cd954714672f440f8cebc68f7b4466cbf1b
Carloscastro16 <andycastro.001016@gmail.com>
2022-07-16 18:51:44 -0500

    Cambios

--- Paginas/pruebas.php
+++ Paginas/pruebas.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Styles/styles.css?v=1">
+    <link rel="stylesheet" href="Styles/styles.css?v=1.1">
     <link rel="stylesheet" href="Styles/argon-dashboard.css?v=1" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" type="text/css">
     
