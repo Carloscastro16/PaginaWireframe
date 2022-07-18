f817beaf916f8ad604cef937240c3d05e3cc4298
Carloscastro16 <andycastro.001016@gmail.com>
2022-07-17 23:35:24 -0500

    Registro evitando email

--- Acciones/sign-in.php
+++ Acciones/sign-in.php
@@ -7,21 +7,29 @@ $agregar = $_POST['agregar'];
     $apellMa= $_POST['apellMa'];
     $correo= $_POST['correo'];
     $pass= $_POST['password'];
-    $nombreEmpresa= $_POST['nombre_empresa'];
+    $nombreEmpresa= $_POST['nombreEmpresa'];
     $telefono= $_POST['telefono'];
     $rfc= $_POST['rfc'];
     $encryptPass = password_hash($pass, PASSWORD_DEFAULT);
     //consulta mysql//
     //mensaje si no se ingresa valores//
+    $consultaEmail = "SELECT COUNT(*) as contador FROM usuario WHERE correo_usuario = '$correo'";
+    $validacion = mysqli_query($conexion, $consultaEmail);
+    $validacionEmail = mysqli_fetch_array($validacion);
 
-if(empty($rfc)){
-    $insertarUsuario= "CALL sp_reg_cliente('$nomUser','$apellPa','$apellMa','$correo','$encryptPass')";
-    $resultados=mysqli_query($conexion,$insertarUsuario);
-    header ('location: ../Paginas/PerfilCliente.php');
-}else{
-    $insertarUsuario = "CALL sp_reg_usrempresa('$nomUser','$apellPa','$apellMa','$correo','$encryptPass',$telefono,'$rfc')";
-    $resultados=mysqli_query($conexion,$insertarUsuario);
-    header ('location: ../Paginas/DashboardEmpresa.php');
-}
+    if($validacionEmail['contador'] == 0){
+        if(empty($rfc)){
+            $insertarUsuario= "CALL sp_reg_cliente('$nomUser','$apellPa','$apellMa','$correo','$encryptPass')";
+            $resultados=mysqli_query($conexion,$insertarUsuario);
+            header ('location: ../Paginas/PerfilCliente.php');
+        }else{
+            $insertarUsuario = "CALL sp_reg_usrempresa('$nomUser','$apellPa','$apellMa','$correo','$encryptPass',$telefono,'$rfc')";
+            $resultados=mysqli_query($conexion,$insertarUsuario);
+            header ('location: ../Paginas/DashboardEmpresa.php');
+        }
+    }else {
+        header ('location: ../Paginas/SignIn.html');
+        echo "<script type='text/javascript> alert('El correo ya existe en la base de datos');</script>";
+    }
     //redireccionamiento//
 ?>
