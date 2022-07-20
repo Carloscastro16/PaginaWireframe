2d077285162858634d355638433c765b77b00b41
Estelaria <mendozae0818@gmail.com>
2022-07-20 00:22:14 -0500

    otras funciones

--- Acciones/registroPaquete.php
+++ Acciones/registroPaquete.php
@@ -1,11 +1,13 @@
 <?php
 include('conec.php');
     session_start();
-    $accion= (isset($_POST['registro']));
+    if(isset($_POST['registro'])){
+    
+    //cargar imagen//
+    //$nomImagenPaquete= $_REQUEST["txtnom"];
     $codusuario=$_POST['fkcodEmpresa'];
-        //cargar imagen//
-    $imagenPaquete= $_FILES['txtFoto']['name'];
-    $ruta= $_FILES["txtFoto"]["tmp_name"];
+    $imagenPaquete= $_FILES['txtnom']['name'];
+    $ruta= $_FILES["txtnom"]["tmp_name"];
     $destino="../imagenes/" . $imagenPaquete;
     copy($ruta,$destino);
     //fin cargar imagen//
@@ -17,6 +19,7 @@ include('conec.php');
     $precioEvento= $_POST['precioEvento'];
     $cantidadPersonas= $_POST['cantidadPersonas'];
     $Descripcion= $_POST['descripcion'];
+<<<<<<< HEAD
     switch($accion){
         case "btnAgregar":
             $registrarPaquete= "INSERT INTO paquete(fk_cod_empresa,imagen_paquete,nom_paquete,fk_cod_tipo_servicio,
@@ -24,18 +27,35 @@ include('conec.php');
     VALUE ('$codusuario',' $imagenPaquete','$nombrePaquete',$tipoServicio,'$locacion','$direccion',' $disponibilidad',
     $precioEvento,'$cantidadPersonas','$Descripcion')";
     
-
-    }
-        
+    $resultados=mysqli_query($conexion,$registrarPaquete);
+=======
+>>>>>>> parent of b4ce980 (Merge branch 'main' of https://github.com/Carloscastro16/PaginaWireframe)
     //conuslta para registrar//
     //$registrarPaquete= "CALL sp_registroPaquete ('$nomImagenPaquete','$nombrePaquete','$tipoServicio',
                                               //  '$locacion','$direccion','$disponibilidad',
                                              //   '$precioEvento','$cantidadPersonas','$Descripcion')";
-    
+<<<<<<< HEAD
+            break;
+      
+    }
+        
 
     
+
+=======
+    $registrarPaquete= "INSERT INTO paquete(fk_cod_empresa,imagen_paquete,nom_paquete,fk_cod_tipo_servicio,
+    fk_cod_ciudad,direc_evento,disponibilidad_evento,precio_paquete,cant_personas,descrip_paquete) 
+    VALUE ('$codusuario',' $imagenPaquete','$nombrePaquete',$tipoServicio,'$locacion','$direccion',' $disponibilidad',
+    $precioEvento,'$cantidadPersonas','$Descripcion')";
     $resultados=mysqli_query($conexion,$registrarPaquete);
-
+    if (!$resultados){
+        echo '<script> alert ("los datos no se insertador") </script>';
+        header('location: index.php');
+    } else {
+        echo '<script> alert("los datos se insertador") </script>';
+    }
+}
+>>>>>>> parent of b4ce980 (Merge branch 'main' of https://github.com/Carloscastro16/PaginaWireframe)
 //redireccionar
 header('location: ../Paginas/altapaquetes.php');
     
