587081fac2386e01c5603411563e6ad5b0c42afc
Carloscastro16 <andycastro.001016@gmail.com>
2022-07-21 13:21:47 -0500

    Revert "Merge branch 'main' of https://github.com/Carloscastro16/PaginaWireframe"
    This reverts commit b4ce9806f7a45d4ec7963c2d25ef585da70b1c18, reversing
    changes made to aa259a08dae5e1f8a31ecffcc0400b0f64655a34.

--- Acciones/registroPaquete.php
+++ Acciones/registroPaquete.php
@@ -1,11 +1,13 @@
 <?php
 include('conec.php');
     session_start();
-    $accion= (isset($_POST['registro']));
+    if(isset($_POST['registro'])){
+    
+    //cargar imagen//
+    //$nomImagenPaquete= $_REQUEST["txtnom"];
     $codusuario=$_POST['fkcodEmpresa'];
-        //cargar imagen//
-    $imagenPaquete= $_FILES['txtFoto']['name'];
-    $ruta= $_FILES["txtFoto"]["tmp_name"];
+    $imagenPaquete= $_FILES['txtnom']['name'];
+    $ruta= $_FILES["txtnom"]["tmp_name"];
     $destino="../imagenes/" . $imagenPaquete;
     copy($ruta,$destino);
     //fin cargar imagen//
@@ -17,25 +19,22 @@ include('conec.php');
     $precioEvento= $_POST['precioEvento'];
     $cantidadPersonas= $_POST['cantidadPersonas'];
     $Descripcion= $_POST['descripcion'];
-    switch($accion){
-        case "btnAgregar":
-            $registrarPaquete= "INSERT INTO paquete(fk_cod_empresa,imagen_paquete,nom_paquete,fk_cod_tipo_servicio,
-    fk_cod_ciudad,direc_evento,disponibilidad_evento,precio_paquete,cant_personas,descrip_paquete) 
-    VALUE ('$codusuario',' $imagenPaquete','$nombrePaquete',$tipoServicio,'$locacion','$direccion',' $disponibilidad',
-    $precioEvento,'$cantidadPersonas','$Descripcion')";
-    
-
-    }
-        
     //conuslta para registrar//
     //$registrarPaquete= "CALL sp_registroPaquete ('$nomImagenPaquete','$nombrePaquete','$tipoServicio',
                                               //  '$locacion','$direccion','$disponibilidad',
                                              //   '$precioEvento','$cantidadPersonas','$Descripcion')";
-    
-
-    
+    $registrarPaquete= "INSERT INTO paquete(fk_cod_empresa,imagen_paquete,nom_paquete,fk_cod_tipo_servicio,
+    fk_cod_ciudad,direc_evento,disponibilidad_evento,precio_paquete,cant_personas,descrip_paquete) 
+    VALUE ('$codusuario',' $imagenPaquete','$nombrePaquete',$tipoServicio,'$locacion','$direccion',' $disponibilidad',
+    $precioEvento,'$cantidadPersonas','$Descripcion')";
     $resultados=mysqli_query($conexion,$registrarPaquete);
-
+    if (!$resultados){
+        echo '<script> alert ("los datos no se insertador") </script>';
+        header('location: index.php');
+    } else {
+        echo '<script> alert("los datos se insertador") </script>';
+    }
+}
 //redireccionar
 header('location: ../Paginas/altapaquetes.php');
     
